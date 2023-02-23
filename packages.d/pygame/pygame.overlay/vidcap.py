3dfff347002d5422c8d5f323881de5825abc90de
pmp-p <mail.peny@free.fr>
2023-02-23 22:58:03 +0100

    start 0.8

---
+++ packages.d/pygame/pygame.overlay/vidcap.py
@@ -0,0 +1,69 @@
+import sys
+import os
+import asyncio
+import pygame
+import pygame.camera
+from pygame.camera import *
+
+del Camera
+
+if sys.platform == "emscripten":
+    import platform
+
+    def get_backends() -> [str]:
+        return ["html5"]
+
+
+    def list_cameras() -> [int]:
+        return ["/dev/video0"]
+
+# TODO: https://toji.dev/webgpu-best-practices/img-textures
+
+
+
+    class Camera:
+        cam = platform.window.MM.camera
+
+        def __init__(self, device, size, format):
+            self.device = device
+            self.width, self.height = size
+            self.format = format
+            self.surface = None
+
+        async def start(self):
+            status = await platform.jsiter(
+                platform.window.MM.camera.init(self.device, self.width, self.height, 0, self.format)
+            )
+            print(f"camera {self.device=} {status=}")
+
+        # queue a image request and pretend we have already the image.
+        async def query_image(self):
+            platform.window.MM.camera.get_raw()
+            if os.path.isfile(self.device):
+                return True
+            await asyncio.sleep(0)
+            return os.path.isfile(self.device)
+
+        def get_image(self, surface = None ):
+            if surface:
+                ...
+            try:
+                return pygame.image.load(self.device)
+            finally:
+                os.unlink(self.device)
+
+        def make_surface(self):
+            if surface is None:
+                ...
+            return self.surface
+
+
+        def get_raw(self):
+            platform.window.MM.camera.get_raw()
+
+else:
+
+    class Camera(pygame.camera.Camera):
+        async def start(self):
+            ...
+
---
+++ test/module/__init__.py
@@ -0,0 +1,1 @@
+print(f"i'm am an imported test module from {__file__=}")
