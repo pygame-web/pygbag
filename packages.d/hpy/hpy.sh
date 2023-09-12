5f218f9e977d2ddf20a7a9c92bc8b574e78046ee
pmp-p <mail.peny@free.fr>
2023-09-12 09:42:40 +0200

    fix hpy object files path for static linking

--- packages.d/hpy/hpy.sh
+++ packages.d/hpy/hpy.sh
@@ -64,41 +64,41 @@ ${SDKROOT}/python3-wasm setup.py install
 . ${SDKROOT}/emsdk/emsdk_env.sh
 
     $SDKROOT/emsdk/upstream/emscripten/emar rcs /opt/python-wasm-sdk/prebuilt/emsdk/libhpy${PYMAJOR}.${PYMINOR}.a \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/debug/src/_debugmod.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/debug/src/autogen_debug_wrappers.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/debug/src/debug_ctx.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/debug/src/debug_ctx_cpython.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/debug/src/debug_handles.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/debug/src/dhqueue.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/debug/src/memprotect.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/debug/src/stacktrace.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/argparse.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/buildvalue.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/ctx_bytes.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/ctx_call.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/ctx_capsule.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/ctx_contextvar.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/ctx_err.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/ctx_eval.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/ctx_listbuilder.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/ctx_long.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/ctx_module.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/ctx_object.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/ctx_tracker.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/ctx_tuple.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/ctx_tuplebuilder.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/ctx_type.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/format.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/helpers.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/devel/src/runtime/structseq.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/trace/src/_tracemod.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/trace/src/autogen_trace_func_table.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/trace/src/autogen_trace_wrappers.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/trace/src/trace_ctx.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/universal/src/ctx.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/universal/src/ctx_meth.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/universal/src/ctx_misc.o \
- build/temp.wasm32-mvp-emscripten-cpython-311/hpy/universal/src/hpymodule.o
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/debug/src/_debugmod.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/debug/src/autogen_debug_wrappers.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/debug/src/debug_ctx.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/debug/src/debug_ctx_cpython.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/debug/src/debug_handles.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/debug/src/dhqueue.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/debug/src/memprotect.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/debug/src/stacktrace.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/argparse.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/buildvalue.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/ctx_bytes.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/ctx_call.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/ctx_capsule.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/ctx_contextvar.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/ctx_err.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/ctx_eval.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/ctx_listbuilder.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/ctx_long.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/ctx_module.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/ctx_object.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/ctx_tracker.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/ctx_tuple.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/ctx_tuplebuilder.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/ctx_type.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/format.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/helpers.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/devel/src/runtime/structseq.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/trace/src/_tracemod.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/trace/src/autogen_trace_func_table.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/trace/src/autogen_trace_wrappers.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/trace/src/trace_ctx.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/universal/src/ctx.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/universal/src/ctx_meth.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/universal/src/ctx_misc.o \
+ build/temp.wasm32-${WASM_FLAVOUR}-emscripten-cpython-311/hpy/universal/src/hpymodule.o
 
 popd
 
