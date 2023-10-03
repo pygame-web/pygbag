d57a940f44bef8e564ec8b55d6bbdb27ab82e180
pmp-p <mail.peny@free.fr>
2023-10-03 21:27:16 +0200

    3.12 has static _ctypes

--- packages.d/_ctypes/_ctypes.sh
+++ packages.d/_ctypes/_ctypes.sh
@@ -16,14 +16,3 @@ echo "
 
 " 1>&2
 
-if pushd /opt/python-wasm-sdk/build/cpython-wasm
-then
-    OBJS="build/temp.emscripten-wasm32-3.11/opt/python-wasm-sdk/src/Python-3.11.5/Modules/_ctypes/_ctypes.o \
- build/temp.emscripten-wasm32-3.11/opt/python-wasm-sdk/src/Python-3.11.5/Modules/_ctypes/callbacks.o \
- build/temp.emscripten-wasm32-3.11/opt/python-wasm-sdk/src/Python-3.11.5/Modules/_ctypes/callproc.o \
- build/temp.emscripten-wasm32-3.11/opt/python-wasm-sdk/src/Python-3.11.5/Modules/_ctypes/cfield.o \
- build/temp.emscripten-wasm32-3.11/opt/python-wasm-sdk/src/Python-3.11.5/Modules/_ctypes/stgdict.o"
-
-    $SDKROOT/emsdk/upstream/emscripten/emar rcs ${SDKROOT}/prebuilt/emsdk/lib_ctypes${PYBUILD}.a $OBJS
-fi
-
