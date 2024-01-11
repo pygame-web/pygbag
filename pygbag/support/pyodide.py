23da477c0ff011f586f6e8841357cda1e8ca8996
pmp-p <mail.peny@free.fr>
2024-01-11 12:33:42 +0100

    emu script

---
+++ pygbag/support/pyodide.py
@@ -0,0 +1,46 @@
+import os
+os.environ['PYODIDE'] = "1"
+
+import sys
+
+import platform
+sys.modules['js'] = platform
+import js
+
+
+class ffi:
+    def create_proxy(self, fn):
+        print(fn)
+        return fn
+
+ffi=ffi()
+
+class m_pyodide:
+    ffi=ffi
+
+sys.modules["pyodide"] = m_pyodide()
+del m_pyodide
+import pyodide
+
+class m_canvas:
+    def getCanvas3D(self, name='canvas',width=1024,height=1024):
+        canvas = platform.document.getElementById("canvas")
+        canvas.width = width
+        canvas.height = height
+        return canvas
+
+class m_pyodide_js:
+    canvas = m_canvas()
+    _module = platform.window
+
+sys.modules["pyodide_js"] = m_pyodide_js()
+del m_canvas
+del m_pyodide_js
+import pyodide_js
+
+
+# fix pygbag bridge
+def jseval(code:str):
+    return platform.window.eval(code)
+
+platform.eval = jseval
